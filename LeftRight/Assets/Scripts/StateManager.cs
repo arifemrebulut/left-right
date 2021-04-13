bb86b514ee54eed0c7ee9071423590e1c6e11712
Emre Bulut <emre@emrebulut.net>
2021-04-13 07:15:13 +0300

    Add two methods to State Manager
    - Two method added to State Manager for changing to current game state or current ball movement state
    
    - ChangeGameState
    - ChangeBallMovementState

--- LeftRight/Assets/Scripts/StateManager.cs
+++ LeftRight/Assets/Scripts/StateManager.cs
@@ -7,4 +7,13 @@ public class StateManager : MonoBehaviour
     public static States.GameState currentGameState { get; private set; }
     public static States.BallMovementState currentBallMovementState { get; private set; }
 
+    public void ChangeGameState(States.GameState desiredGameState)
+    {
+        currentGameState = desiredGameState;
+    }
+
+    private void ChangeBallMovementState(States.BallMovementState desiredBallMovementState)
+    {
+        currentBallMovementState = desiredBallMovementState;
+    }
 }
