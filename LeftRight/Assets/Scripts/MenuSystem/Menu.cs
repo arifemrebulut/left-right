ef9a0784252e133cbc48743e0e5abd1bfb914a31
Emre Bulut <emre@emrebulut.net>
2021-04-13 21:00:26 +0300

    Create MenuController script
    - MenuController script has a public variable type of MenuType and each Menu in the game will have this this script

--- LeftRight/Assets/Scripts/MenuSystem/Menu.cs
+++ LeftRight/Assets/Scripts/MenuSystem/Menu.cs
@@ -1,14 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class Menu : MonoBehaviour
+
+public enum MenuType
 {
-    public enum MenuType
-    {
-        MainMenu,
-        GameUI,
-        PauseMenu,
-        GameOverScreen
-    }
+    MainMenu,
+    GameUI,
+    PauseMenu,
+    GameOverScreen
 }
