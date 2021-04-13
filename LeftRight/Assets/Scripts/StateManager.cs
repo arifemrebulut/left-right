7b37b78484a4befd2ac86bd3ba21c1ab0d2e6f3c
Emre Bulut <emre@emrebulut.net>
2021-04-13 07:02:19 +0300

    Create States and StateManaget Scripts
    - States script is holding enums for game and ball movement states and these are :
    - GameState
    -- OnStart
    -- OnPlay
    -- OnGameOver
    
    - BallMovementState
    -- Patrol
    -- Left
    -- Right

---
+++ LeftRight/Assets/Scripts/StateManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManager : MonoBehaviour
+{
+    public static States.GameState currentGameState { get; private set; }
+    public static States.BallMovementState currentBallMovementState { get; private set; }
+
+}
---
+++ LeftRight/Assets/Scripts/States.cs
@@ -0,0 +1,18 @@
+
+
+public class States
+{
+    public enum GameState
+    {
+        OnStart,
+        OnPlay,
+        OnGameOver
+    }
+
+    public enum BallMovementState
+    {
+        Patrol,
+        Left,
+        Right
+    }
+}
