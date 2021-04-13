c1d343e502f0799c4757d973ad92b2fcccff01ba
Emre Bulut <emre@emrebulut.net>
2021-04-13 20:54:23 +0300

    Create Menu script
    - Menu script will hold MenuType enum

---
+++ LeftRight/Assets/Scripts/MenuSystem/Menu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public enum MenuType
+    {
+        MainMenu,
+        GameUI,
+        PauseMenu,
+        GameOverScreen
+    }
+}
