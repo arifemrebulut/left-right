b3faf5e809c83bb225c29b23ace7e9035b3ca7f9
Emre Bulut <80202387+arifemrebulut@users.noreply.github.com>
2021-04-27 19:09:10 +0300

    Upgrade project to Unity 2021.1.4f1

--- LeftRight/Assets/Scripts/GameManager.cs
+++ LeftRight/Assets/Scripts/GameManager.cs
@@ -1,27 +1,36 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject startingPoint;
     [SerializeField] private List<GameObject> levels;
+    [SerializeField] private Transform levelsParent;
 
-    private GameObject previousLevel, currentLevel, nextLevel;
+    private GameObject currentLevel, nextLevel;
+    private List<GameObject> instatiatedLevels;
 
     #region Subscribing and Unsubscribing to events
     private void OnEnable()
     {
         EventBroker.OnCreaterLevelEndPoint += CreateRandomLevel;
+        EventBroker.OnGameRestart += DeleteLevels;
+        EventBroker.OnGameRestart += CreateStartingPoint;
     }
 
     private void OnDestroy()
     {
         EventBroker.OnCreaterLevelEndPoint -= CreateRandomLevel;
+        EventBroker.OnGameRestart -= DeleteLevels;
+        EventBroker.OnGameRestart -= CreateStartingPoint;
     }
     #endregion
 
     private void Start()
     {
+        instatiatedLevels = new List<GameObject>();
+
         CreateStartingPoint();
 
         currentLevel = startingPoint;
@@ -35,11 +44,15 @@ public class GameManager : MonoBehaviour
 
     private void CreateStartingPoint()
     {
-        Instantiate(startingPoint, transform.position, Quaternion.identity);
+        GameObject startingLevel = Instantiate(startingPoint, transform.position, Quaternion.identity);
+
+        instatiatedLevels.Add(startingLevel);
 
         Vector3 firstLevelPosition = new Vector3(0, startingPoint.transform.position.y + 40, 0);
 
         GameObject firstLevel = Instantiate(GetRandomLevel(), firstLevelPosition, Quaternion.identity);
+
+        instatiatedLevels.Add(firstLevel);
 
         currentLevel = firstLevel;
     }
@@ -51,6 +64,8 @@ public class GameManager : MonoBehaviour
         Vector3 newLevelPosition = new Vector3(0, currentLevel.transform.position.y + 40, 0);
 
         nextLevel = Instantiate(GetRandomLevel(), newLevelPosition, Quaternion.identity);
+
+        instatiatedLevels.Add(nextLevel);
     }
 
     private GameObject GetRandomLevel()
@@ -60,5 +75,13 @@ public class GameManager : MonoBehaviour
         GameObject randomLevel = levels[randomIndex];
 
         return randomLevel;
+    }
+
+    private void DeleteLevels()
+    {
+        foreach (var level in instatiatedLevels)
+        {
+            Destroy(level.gameObject);
+        }
     }
 }
