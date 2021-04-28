6dbd122a36c89120da520af5728a5c3faa1827f5
Emre Bulut <80202387+arifemrebulut@users.noreply.github.com>
2021-04-28 06:13:56 +0300

    Add a countdown to ObsticleHorizontalMovement

--- LeftRight/Assets/ObsticleHorizontalMovement.cs
+++ LeftRight/Assets/ObsticleHorizontalMovement.cs
@@ -10,21 +10,40 @@ public class ObsticleHorizontalMovement : MonoBehaviour
 {
     [SerializeField] private float movementSpeed;
     [SerializeField] private MovementDirection movementDirection;
+    [SerializeField] private float triggerTimeInSeconds;
+
+    private bool canMove = false;
 
     void Update()
     {
+        CountDown();
+
         MoveObsticle();
     }
 
     private void MoveObsticle()
     {
-        if (movementDirection == MovementDirection.Left)
+        if (movementDirection == MovementDirection.Left && canMove)
         {
             transform.Translate(Vector2.left * movementSpeed * Time.deltaTime, Space.World);
         }
-        else
+        else if (movementDirection == MovementDirection.Right && canMove)
         {
             transform.Translate(Vector2.right * movementSpeed * Time.deltaTime, Space.World);
+        }
+    }
+
+    private void CountDown()
+    {
+        triggerTimeInSeconds -= Time.deltaTime;
+
+        if (triggerTimeInSeconds <= 0)
+        {
+            canMove = true;
+        }
+        else
+        {
+            canMove = false;
         }
     }
 }
